c70417d903322405995f90f25fc724707e65abc4
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2021-05-03 10:14:37 +0200

    asd

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:4200',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  });
-  const configService: ConfigService = app.get(ConfigService);
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
