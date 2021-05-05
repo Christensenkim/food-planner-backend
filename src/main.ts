0149432eff745be11e8d498c07b601fa44899bd5
Christensenkim <55380386+Christensenkim@users.noreply.github.com>
2021-05-05 09:22:16 +0200

    asd

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   const configService: ConfigService = app.get(ConfigService);
   await app.listen(process.env.PORT || 8080);
 }
